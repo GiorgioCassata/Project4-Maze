22e529f521f68d28414bd967a64089fcaa521311
GiorgioCassata <38116994+GiorgioCassata@users.noreply.github.com>
2019-04-12 12:23:14 -0600

    File in works, graph representation needs to be revised

--- main.cpp
+++ main.cpp
@@ -7,12 +7,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 #include <cmath>
 
 using namespace std;
 
 enum Company {RED, BLUE, GREEN, NOCOMPANY};
 enum LineType {HORSE, CABLE, TROLLEY, BUS, NOLINETYPE};
+
+string comp_to_string(Company comp){
+        switch (comp) {
+        case RED:
+            return "RED";
+        case BLUE:
+            return "BLUE";
+        case GREEN:
+            return "GREEN";
+        default:
+            return "NONE";
+        }
+        return "ERROR";
+}
 
 class City {
     private:
@@ -29,18 +44,42 @@ class City {
         City* seven;
 
         City() {
-            parent = NULL;
-            one = NULL;
-            two = NULL;
-            three = NULL;
-            four = NULL;
-            five = NULL;
-            six = NULL;
-            seven = NULL;
-            _parentCompany = NOCOMPANY;
-            _parentLineType = NOLINETYPE;
+            this->parent = NULL;
+            this->one = NULL;
+            this->two = NULL;
+            this->three = NULL;
+            this->four = NULL;
+            this->five = NULL;
+            this->six = NULL;
+            this->seven = NULL;
+            this->_parentCompany = NOCOMPANY;
+            this->_parentLineType = NOLINETYPE;
         }
 
+        City(City *parent, Company company, LineType line) {
+            this->parent = parent;
+            this->one = NULL;
+            this->two = NULL;
+            this->three = NULL;
+            this->four = NULL;
+            this->five = NULL;
+            this->six = NULL;
+            this->seven = NULL;
+            this->_parentCompany = company;
+            this->_parentLineType = line;
+        }
+
+        /*
+        ~City() {
+            if (this->one != NULL) delete one;
+            if (this->two != NULL) delete two;
+            if (this->three != NULL) delete three;
+            if (this->four != NULL) delete four;
+            if (this->five != NULL) delete five;
+            if (this->six != NULL) delete six;
+            if (this->seven != NULL) delete seven;
+        }
+        */
         ~City() {
             delete one;
             delete two;
@@ -49,6 +88,16 @@ class City {
             delete five;
             delete six;
             delete seven;
+        }
+
+        void insert(City *x) {
+            if (this->one == NULL) this->one = x;
+            if (this->two == NULL) this->two = x;
+            if (this->three == NULL) this->three = x;
+            if (this->four == NULL) this->four = x;
+            if (this->five == NULL) this->five = x;
+            if (this->six == NULL) this->six = x;
+            if (this->seven == NULL) this->seven = x;
         }
 
         Company getC() {
@@ -63,8 +112,12 @@ class City {
 
 int main() {
     int numTowns, numTransitLines;
+    char parent, child, comp, trans;
+    Company company;
+    LineType transit;
 
-
+    map<char, City*> cities;
+    map<char, City*>::iterator it;
 
     ifstream fin;
     ofstream fout;
@@ -74,24 +127,85 @@ int main() {
     fin >> numTowns;
     fin >> numTransitLines;
 
-    while (!fin.eof()) {
-        fin >>
-    }
+    // initiate root (aka startsville)
+    City *startsville = new City();
+    cities.emplace('A', startsville);
     // read input and create graph
+    while (!fin.eof()) {
+        fin >> parent;
+        fin >> child;
+        fin >> comp;
+        fin >> trans;
+
+        switch (comp) {
+            case 'R':
+                company = RED;
+                break;
+            case 'B':
+                company = BLUE;
+                break;
+            case 'G':
+                company = GREEN;
+                break;
+            default:
+                company = NOCOMPANY;
+        }
+        switch (trans) {
+            case 'H':
+                transit = HORSE;
+                break;
+            case 'C':
+                transit = CABLE;
+                break;
+            case 'T':
+                transit = TROLLEY;
+                break;
+            case 'B':
+                transit = BUS;
+                break;
+            default:
+                transit = NOLINETYPE;
+        }
+
+        // parent should already exist
+        it = cities.find(parent);
+        if (it == cities.end()) {
+            cout << "ERROR: Discontinuity in input file" << endl;
+            return -1;
+        }
+
+        // if parent exists check if child node exists
+        // if edge already exists continue
+        it = cities.find(child);
+        if (it == cities.end()) {
+            // create new node at the first null child
+            City *cityNode = new City(cities.at(parent), company, transit);
+            cities.emplace(child, cityNode);
+            cities.at(parent)->insert(cityNode);
+        } else {
+            //City *cityNode = cities.at(child);
+            //cities.at(parent)->insert(cityNode);
+            continue;
+        }
+
+    }
 
     fin.close();
 
-    City *startsville = new City;
-    if (startsville->getC() == NOCOMPANY) {
-        cout << "No city lmao" << endl;
+    cout << cities.size() << endl;
+    for (int i = 0; i < cities.size()+5; ++i) {
+        if (cities[i] != 0x0) {
+            cout << comp_to_string(cities[i]->getC()) << endl;
+        }
     }
-    delete startsville;
+
+
 
     // generate and save output to external file & print to console
     fout.open("output.txt");
 
     fout.close();
 
-
+    delete startsville; // deleting root should cause deconstructors for each node to be deleted
     return 0;
 }
