ac0bc3b27cc34ffa0e77b17c570c104df1df599a
GiorgioCassata <38116994+GiorgioCassata@users.noreply.github.com>
2019-04-12 09:41:59 -0600

    Set up class for city nodes

--- main.cpp
+++ main.cpp
@@ -11,8 +11,58 @@
 
 using namespace std;
 
+enum Company {RED, BLUE, GREEN, NOCOMPANY};
+enum LineType {HORSE, CABLE, TROLLEY, BUS, NOLINETYPE};
+
+class City {
+    private:
+        Company _parentCompany;
+        LineType _parentLineType;
+    public:
+        City* parent;
+        City* one;
+        City* two;
+        City* three;
+        City* four;
+        City* five;
+        City* six;
+        City* seven;
+
+        City() {
+            parent = NULL;
+            one = NULL;
+            two = NULL;
+            three = NULL;
+            four = NULL;
+            five = NULL;
+            six = NULL;
+            seven = NULL;
+            _parentCompany = NOCOMPANY;
+            _parentLineType = NOLINETYPE;
+        }
+
+        ~City() {
+            delete one;
+            delete two;
+            delete three;
+            delete four;
+            delete five;
+            delete six;
+            delete seven;
+        }
+
+        Company getC() {
+            return this->_parentCompany;
+        }
+
+        LineType getL() {
+            return this->_parentLineType;
+        }
+};
+
+
 int main() {
-    int numDays, numTransitLines;
+    int numTowns, numTransitLines;
 
 
 
@@ -22,12 +72,17 @@ int main() {
     // open input file in which the first line has num of towns and num of transit lines
     file.open("input.txt");
     file >> numTowns;
-    file >> numTransitLines
+    file >> numTransitLines;
 
     // read input and create graph
 
     file.close();
 
+    City *startsville = new City;
+    if (startsville->getC() == NOCOMPANY) {
+        cout << "No city lmao" << endl;
+    }
+    delete startsville;
 
     // generate and save output to external file & print to console
     fout.open("output.txt");
